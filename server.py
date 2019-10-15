e17c7f13fde010c84ddabe12b0261dfab9926fcd
Ben Chapman-Kish <ben.chapmankish@gmail.com>
2019-10-15 08:42:02 -0400

    Complete server logic

--- server.py
+++ server.py
@@ -3,14 +3,22 @@ import sqlite3 as sql
 
 app = Flask(__name__)
 
+# Developer method, not for actual prod use
 @app.route("/initialize")
 def initialize():
     conn = sql.connect("database.db")
     
     cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS products")
     cur.execute("CREATE TABLE products (name TEXT, imgpath TEXT, price INTEGER, stock INTEGER)")
-    cur.execute("INSERT INTO products (name, imgpath, price, stock) VALUES ('Guitar', '/images/guitar.jpg', 12000, 6)")
-    return redirect('/')
+    cur.execute("""INSERT INTO products (name, imgpath, price, stock) VALUES \
+        ('Guitar', 'images/guitar.jpg', 12000, 8), \
+        ('Couch', 'images/couch.jpg', 24000, 2), \
+        ('Plant', 'images/plant.jpg', 1800, 34), \
+        ('Phone', 'images/phone.png', 85000, 0)
+    """)
+    conn.commit()
+    return render_template("message.html", message="Initialized database.")
 
 @app.route("/products")
 @app.route("/")
@@ -21,29 +29,46 @@ def home_page():
     cur.execute("SELECT rowid, * FROM products")
     
     rows = cur.fetchall()
-    return render_template("index.html", products = rows)
+    
+    products = []
+    for row in rows:
+        products.append({
+            "id":    row[0],
+            "name":  row[1],
+            "src":   "/static/" + row[2],
+            "price": "$" + str(row[3]/100.0),
+            "stock": str(row[4]) + " left",
+        })
+    
+    for product in products:
+        print(product)
+
+    return render_template("index.html", products = products)
 
 @app.route("/purchase_complete")
 def purchase_complete():
     return render_template("purchase_complete.html")
 
-@app.route("/buy/<product_id>", methods=['POST'])
+@app.route("/buy/<product_id>")
 def buy(product_id):
     if not product_id:
-        return "<p>Invalid product ID!</p>"
+        return render_template("message.html", message="Invalid product ID!")
 
     conn = sql.connect("database.db")
     
     cur = conn.cursor()
     cur.execute("SELECT stock FROM products WHERE rowid = " + str(product_id))
-    stock = cur.fetchone()[0]
+    stock = cur.fetchone()
+    if not stock:
+        return render_template("message.html", message="Invalid product ID!")
 
-    if (stock.isdigit() and int(stock) > 0):
+    if (stock[0] > 0):
         # If this was a real app, this is where we'd process the money transaction
         cur.execute("UPDATE products SET stock = stock - 1 WHERE rowid = " + str(product_id))
-        return redirect('/purchase_complete')
+        conn.commit()
+        return render_template("message.html", message="Purchase successful!")
     
-    return "<p>Insufficient stock!</p>"
+    return render_template("message.html", message="Insufficient stock!")
 
 if __name__ == '__main__':
    app.run(debug = True)
