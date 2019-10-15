b32733d8e4751d1a48dc91d03dfea54224022f74
Ben Chapman-Kish <ben.chapmankish@gmail.com>
2019-10-15 09:09:01 -0400

    Add tracking of transactions

--- server.py
+++ server.py
@@ -3,12 +3,15 @@ import sqlite3 as sql
 
 app = Flask(__name__)
 
-# Developer method, not for actual prod use
-@app.route("/initialize")
+@app.route("/reset")
+def reset():
+    initialize()
+    return render_template("message.html", message="Database reset.")
+
 def initialize():
     conn = sql.connect("database.db")
-    
     cur = conn.cursor()
+
     cur.execute("DROP TABLE IF EXISTS products")
     cur.execute("CREATE TABLE products (name TEXT, imgpath TEXT, price INTEGER, stock INTEGER)")
     cur.execute("""INSERT INTO products (name, imgpath, price, stock) VALUES \
@@ -17,8 +20,12 @@ def initialize():
         ('Plant', 'images/plant.jpg', 1800, 34), \
         ('Phone', 'images/phone.png', 85000, 0)
     """)
+
+    cur.execute("DROP TABLE IF EXISTS transactions")
+    cur.execute("CREATE TABLE transactions (timestamp TEXT, productid INTEGER, value INTEGER)")
+    
     conn.commit()
-    return render_template("message.html", message="Initialized database.")
+    print("Initialized database")
 
 @app.route("/products")
 @app.route("/")
@@ -29,6 +36,8 @@ def home_page():
     cur.execute("SELECT rowid, * FROM products")
     
     rows = cur.fetchall()
+
+    print("Retrieved " + str(len(rows)) + " database entries")
     
     products = []
     for row in rows:
@@ -40,10 +49,14 @@ def home_page():
             "stock": str(row[4]) + " left",
         })
     
-    for product in products:
-        print(product)
+    cur.execute("SELECT SUM(value) FROM transactions")
+    result = cur.fetchone()[0]
+    earnings = 0
 
-    return render_template("index.html", products = products)
+    if result:
+        earnings = result/100.0
+
+    return render_template("index.html", products = products, earnings = earnings)
 
 @app.route("/purchase_complete")
 def purchase_complete():
@@ -57,13 +70,18 @@ def buy(product_id):
     conn = sql.connect("database.db")
     
     cur = conn.cursor()
-    cur.execute("SELECT stock FROM products WHERE rowid = " + str(product_id))
-    stock = cur.fetchone()
-    if not stock:
+    cur.execute("SELECT rowid, price, stock FROM products WHERE rowid = " + str(product_id))
+    result = cur.fetchone()
+    if not result:
         return render_template("message.html", message="Invalid product ID!")
 
-    if (stock[0] > 0):
-        # If this was a real app, this is where we'd process the money transaction
+    (rowid, price, stock) = result
+
+    if (stock > 0):
+        print("Processed transaction of value $" + str(price/100.0))
+        cur.execute("INSERT INTO transactions (timestamp, productid, value) VALUES " + \
+            "(datetime(), "+str(rowid)+", "+str(price)+")")
+
         cur.execute("UPDATE products SET stock = stock - 1 WHERE rowid = " + str(product_id))
         conn.commit()
         return render_template("message.html", message="Purchase successful!")
@@ -71,4 +89,5 @@ def buy(product_id):
     return render_template("message.html", message="Insufficient stock!")
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    initialize()
+    app.run(debug = True)
